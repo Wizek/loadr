a830791e42d64b95b3c410e20534752a86b0d085
Milán Nagy <123.wizek@gmail.com>
2013-03-12 23:04:07 +0100

    memCache: teaked naming

--- spec/memCache.spec.js
+++ spec/memCache.spec.js
@@ -23,15 +23,15 @@ describe('in-memory cache', function() {
   it('should keep track of time', function() {
     time = 1
     memCache.set('b', 1)
-    expect(memCache._store.b).toBeSupersetOf({value:1,setAt:1,lastGetAt:1})
+    expect(memCache._store.b).toBeSupersetOf({value:1,lastSet:1,lastGet:1})
 
     time = 2
     memCache.get('b')
-    expect(memCache._store.b).toBeSupersetOf({value:1,setAt:1,lastGetAt:2})
+    expect(memCache._store.b).toBeSupersetOf({value:1,lastSet:1,lastGet:2})
 
     time = 3
     memCache.set('b', 2)
-    expect(memCache._store.b).toBeSupersetOf({value:2,setAt:3,lastGetAt:3})
+    expect(memCache._store.b).toBeSupersetOf({value:2,lastSet:3,lastGet:3})
   })
 
   it('should keep track of hits', function() {
