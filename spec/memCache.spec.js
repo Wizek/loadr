166d059acb16f0db98fa2ea74d0052713d1b7591
Milán Nagy <123.wizek@gmail.com>
2013-03-12 22:30:04 +0100

    memCache: added support for keeping track of number hits (gets)

--- spec/memCache.spec.js
+++ spec/memCache.spec.js
@@ -23,14 +23,28 @@ describe('in-memory cache', function() {
   it('should keep track of time', function() {
     time = 1
     memCache.set('b', 1)
-    expect(memCache._store).toEqual({b:{value:1,set_at:1,got_at:1}})
+    expect(memCache._store.b).toBeSupersetOf({value:1,setAt:1,lastGetAt:1})
 
     time = 2
     memCache.get('b')
-    expect(memCache._store).toEqual({b:{value:1,set_at:1,got_at:2}})
+    expect(memCache._store.b).toBeSupersetOf({value:1,setAt:1,lastGetAt:2})
 
     time = 3
     memCache.set('b', 2)
-    expect(memCache._store).toEqual({b:{value:2,set_at:3,got_at:3}})
+    expect(memCache._store.b).toBeSupersetOf({value:2,setAt:3,lastGetAt:3})
+  })
+
+  it('should keep track of hits', function() {
+    memCache.set('b', 1)
+    expect(memCache._store.b.hits).toEqual(0)
+
+    memCache.get('b')
+    expect(memCache._store.b.hits).toEqual(1)
+
+    memCache.get('b')
+    expect(memCache._store.b.hits).toEqual(2)
+
+    memCache.set('b', 1)
+    expect(memCache._store.b.hits).toEqual(0)
   })
 })
